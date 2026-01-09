78554298c64695f398daa1805022a6df22e3aabc
Make <71177618+Make1205@users.noreply.github.com>
2026-01-09 17:08:16 +0800

    Fuse basemul accumulate in NTT-domain multiplies

--- src/avx2/poly.c
+++ src/avx2/poly.c
@@ -151,11 +151,9 @@ void avx_poly_matrix_vec_mul(poly_vec *res, const poly_matrix *A, const poly_vec
 void avx_poly_matrix_vec_mul_ntt(poly_vec *res, const poly_matrix *A_ntt, const poly_vec *s_ntt) {
     for(int i=0; i<MLWQ_K; ++i) {
         poly acc;
-        poly prod;
         avx_basemul(acc.coeffs, A_ntt->row[i].vec[0].coeffs, s_ntt->vec[0].coeffs);
         for(int j=1; j<MLWQ_K; ++j) {
-            avx_basemul(prod.coeffs, A_ntt->row[i].vec[j].coeffs, s_ntt->vec[j].coeffs);
-            avx_poly_add_inplace(&acc, &prod);
+            avx_basemul_acc(acc.coeffs, A_ntt->row[i].vec[j].coeffs, s_ntt->vec[j].coeffs);
         }
         avx_invntt(acc.coeffs);
         avx_reduce(acc.coeffs);
@@ -206,11 +204,9 @@ void avx_poly_vec_transpose_mul(poly *res, const poly_vec *a_t, const poly_vec *
 
 void avx_poly_vec_transpose_mul_ntt(poly *res, const poly_vec *a_ntt, const poly_vec *b_ntt) {
     poly acc;
-    poly prod;
     avx_basemul(acc.coeffs, a_ntt->vec[0].coeffs, b_ntt->vec[0].coeffs);
     for(int i=1; i<MLWQ_K; ++i) {
-        avx_basemul(prod.coeffs, a_ntt->vec[i].coeffs, b_ntt->vec[i].coeffs);
-        avx_poly_add_inplace(&acc, &prod);
+        avx_basemul_acc(acc.coeffs, a_ntt->vec[i].coeffs, b_ntt->vec[i].coeffs);
     }
     avx_invntt(acc.coeffs);
     avx_reduce(acc.coeffs);
